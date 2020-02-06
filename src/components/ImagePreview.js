8b0367bca6e1ebd6729eac2a4eef90a68628e166
Amy Hooper <amy.hooper@hotmail.co.uk>
2020-02-06 15:24:19 +0000

    Put the inspiration on the screen where it belongs

--- src/components/ImagePreview.js
+++ src/components/ImagePreview.js
@@ -1,15 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { inspiration } from '../miscellany/inspiration';
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (Math.floor(max)));
+}
 
 export function ImagePreview({ src, alt, filter }) {
   const [className, setClassName] = useState('inkwell');
 
+  const [inspired, setInspired] = useState(false);
+
+  const [quote, setQuote] = useState('Busy Actualizing, come back later');
+
+  useEffect(() => {
+    setQuote(inspiration.quotes[getRandomInt(inspiration.quotes.length)]);
+  }, []);
+
+  const onClick = () => setInspired(!inspired);
+
   return (
-    <img
-      className={className}
-      src={src}
-      alt={alt}
-      onMouseEnter={() => setClassName(filter)}
-      onMouseLeave={() => setClassName('inkwell')}
-    />
+    <div className="relative rounded shadow-lg overflow-hidden bg-white">
+      <button
+        onMouseEnter={() => setClassName(filter)}
+        onMouseLeave={() => setClassName('inkwell')}
+        onClick={onClick}
+        className="h-full w-full"
+      >
+        <img
+          className={`${className} ${inspired ? 'opacity-25' : ''} transition duration-500 ease-in-out h-full w-full`}
+          src={src}
+          alt={alt}
+        />
+      </button>
+      {inspired && (
+        <button
+          onClick={onClick}
+          className="absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center"
+          type="button"
+        >
+          {quote}
+        </button>
+      )}
+    </div>
   );
 }
