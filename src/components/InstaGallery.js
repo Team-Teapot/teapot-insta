812652327786fd0379a7f90fd1c6cb5590682020
Luke Robinson <robinsonlm@live.co.uk>
2020-02-06 11:08:56 +0000

    Set up tailwind css

--- src/components/InstaGallery.js
+++ src/components/InstaGallery.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 export function InstaGallery() {
   return (
-    <div>
-      Instagalllery
+    <div className="flex flex-wrap mb-4">
+      <div className="w-full sm:w-1/2 md:w-1/3 bg-blue-100 h-12">Instagallery</div>
+      <div className="w-full sm:w-1/2 md:w-1/3 bg-blue-100 h-12">Instagallery</div>
+      <div className="w-full sm:w-1/2 md:w-1/3 bg-blue-100 h-12">Instagallery</div>
+      <div className="w-full sm:w-1/2 md:w-1/3 bg-blue-100 h-12">Instagallery</div>
+      <div className="w-full sm:w-1/2 md:w-1/3 bg-blue-100 h-12">Instagallery</div>
+      <div className="w-full sm:w-1/2 md:w-1/3 bg-blue-100 h-12">Instagallery</div>
+      <div className="w-full sm:w-1/2 md:w-1/3 bg-blue-100 h-12">Instagallery</div>
+      <div className="w-full sm:w-1/2 md:w-1/3 bg-blue-100 h-12">Instagallery</div>
+      <div className="w-full sm:w-1/2 md:w-1/3 bg-blue-100 h-12">Instagallery</div>
+      <div className="w-full sm:w-1/2 md:w-1/3 bg-blue-100 h-12">Instagallery</div>
+      <div className="w-full sm:w-1/2 md:w-1/3 bg-blue-100 h-12">Instagallery</div>
+      <div className="w-full sm:w-1/2 md:w-1/3 bg-blue-100 h-12">Instagallery</div>
     </div>
   );
 }
