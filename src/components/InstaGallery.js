ff4d112a118f4da6617815844a21be431a1d7c5a
Luke Robinson <robinsonlm@live.co.uk>
2020-02-06 11:43:53 +0000

    Added images to the insta gallery

--- src/components/InstaGallery.js
+++ src/components/InstaGallery.js
@@ -4,18 +4,51 @@ import { Grid, GridItem } from './Grid';
 export function InstaGallery() {
   return (
     <Grid>
-      <GridItem><div className="bg-blue-300"> Instagallery</div></GridItem>
-      <GridItem><div className="bg-blue-300"> Instagallery</div></GridItem>
-      <GridItem><div className="bg-blue-300"> Instagallery</div></GridItem>
-      <GridItem><div className="bg-blue-300"> Instagallery</div></GridItem>
-      <GridItem><div className="bg-blue-300"> Instagallery</div></GridItem>
-      <GridItem><div className="bg-blue-300"> Instagallery</div></GridItem>
-      <GridItem><div className="bg-blue-300"> Instagallery</div></GridItem>
-      <GridItem><div className="bg-blue-300"> Instagallery</div></GridItem>
-      <GridItem><div className="bg-blue-300"> Instagallery</div></GridItem>
-      <GridItem><div className="bg-blue-300"> Instagallery</div></GridItem>
-      <GridItem><div className="bg-blue-300"> Instagallery</div></GridItem>
-      <GridItem><div className="bg-blue-300"> Instagallery</div></GridItem>
+      <GridItem>
+        <div className="bg-blue-300">
+          <img src="/Art.jpg" alt="Art!"/>
+        </div>
+      </GridItem>
+      <GridItem>
+        <div className="bg-blue-300">
+          <img src="/Family_Photo.jpg" alt="Family of teapots"/>
+        </div>
+      </GridItem>
+      <GridItem>
+        <div className="bg-blue-300">
+          <img src="/Teapot_on_Windowsill.jpg" alt="Teapot on a windowsill"/>
+        </div>
+      </GridItem>
+      <GridItem>
+        <div className="bg-blue-300">
+          <img src="/Art.jpg" alt="Art!"/>
+        </div>
+      </GridItem>
+      <GridItem>
+        <div className="bg-blue-300">
+          <img src="/Family_Photo.jpg" alt="Family of teapots"/>
+        </div>
+      </GridItem>
+      <GridItem>
+        <div className="bg-blue-300">
+          <img src="/Teapot_on_Windowsill.jpg" alt="Teapot on a windowsill"/>
+        </div>
+      </GridItem>
+      <GridItem>
+        <div className="bg-blue-300">
+          <img src="/Art.jpg" alt="Art!"/>
+        </div>
+      </GridItem>
+      <GridItem>
+        <div className="bg-blue-300">
+          <img src="/Family_Photo.jpg" alt="Family of teapots"/>
+        </div>
+      </GridItem>
+      <GridItem>
+        <div className="bg-blue-300">
+          <img src="/Teapot_on_Windowsill.jpg" alt="Teapot on a windowsill"/>
+        </div>
+      </GridItem>
     </Grid>
   );
 }
