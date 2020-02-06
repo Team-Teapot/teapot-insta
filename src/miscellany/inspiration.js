306ec1814c8c6efd29144b78b51ad0670fd30a59
Amy Hooper <amy.hooper@hotmail.co.uk>
2020-02-06 14:45:49 +0000

    Inspiration

---
+++ src/miscellany/inspiration.js
@@ -0,0 +1,10 @@
+export const inspiration = {
+  quotes: [
+    'It\'s all just computers, innit',
+    'Do the bare minz',
+    'If one does not want to become someone else, there is only one possibility: get hit in the head with an hammer. Repeatedly and work.',
+    'Every day is a school day',
+    'I\'m sorry, but the colour scheme is making me want to cry',
+    'How old were YOU when you first learnt what a crustacean was?',
+  ],
+};
