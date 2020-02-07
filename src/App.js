f76555231d2bd21d3f8a32f21a700c9bf219738a
Luke Robinson <robinsonlm@live.co.uk>
2020-02-07 10:12:13 +0000

    Set up routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
-import { InstaGallery } from './components/InstaGallery';
+import React from "react";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { InstaGallery } from "./components/InstaGallery";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="bg-blue-300 h-screen">
-      <Header />
-      <div className="flex pt-16">
-        <InstaGallery />
+    <Router>
+      <div className="bg-blue-300 h-screen">
+        <Header />
+        <div className="flex pt-16">
+          <Switch>
+            <Route path="/" exact>
+              <InstaGallery />
+            </Route>
+            <Route path="/ali">
+              <div>Ali's site here</div>
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
