fd3ceaa9fb251d763ae7336754855c58eb150b11
Luke Robinson <robinsonlm@live.co.uk>
2020-02-07 09:08:52 +0000

    Bold

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="bg-blue-300 h-screen">
       <h1 className="w-full bg-gray-800 text-white p-4 fixed top-0 z-10 flex">
         <img src="/logo.svg" alt="Logo of a teapot" className="h-10 w-10"/>
-        <span className="pl-4">Over-Engineered Teapot</span>
+        <span className="pl-4 font-bold">Over-Engineered Teapot</span>
       </h1>
       <div className="flex pt-16">
         <InstaGallery />
